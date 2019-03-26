5f69f28bf86cf3727a7298014eb92b60ffd64daa
luisgonzalez1 <39278497+luisgonzalez1@users.noreply.github.com>
2019-03-26 14:18:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'This is a test of webhook!'
+    title: 'This is a test of webhook!!'
     })
     res.status(200).end();
 })
